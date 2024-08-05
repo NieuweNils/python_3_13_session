7e8fd0907200aaaf5ad13099e1832adf050adfc2
nils <nils.nieuwenkamp@eneco.com>
2024-08-05 15:35:41 +0200

    add CPU-bound computation
    calculate a recursive (fibonacci) function instead of sleeping

--- free_threading.py
+++ free_threading.py
@@ -98,6 +98,10 @@ class Data:
     context: str
 
 
+def calculate_fibonaci_sequence(n):
+    return n if n < 2 else calculate_fibonaci_sequence(n - 2) + calculate_fibonaci_sequence(n - 1)
+
+
 class awesomeProcessor(threading.Thread):
     def __init__(self, uuid: str, event: threading.Event):
         super().__init__(name=f"Data Processor {uuid}", daemon=False, args=(event,))
@@ -112,33 +116,34 @@ class awesomeProcessor(threading.Thread):
     # Overrides threading.Thread.run()
     def run(self):
         while not self.event.is_set():
-            self.queue.get()
+            data_to_process = self.queue.get()
             self.counter += 1
-            time.sleep(0.01)
+            calculate_fibonaci_sequence(data_to_process.number)
             if self.queue.empty():
                 self.event.set()
 
 
 def generate_data(number_of_records: int):
-    for i in range(number_of_records):
-        yield random.randint(0, 10), random.choice(HAIKUS)
+    for _ in range(number_of_records):
+        yield random.randint(20, 25), random.choice(HAIKUS)
 
 
 if __name__ == "__main__":
     for i in range(10):
         start_time = time.time()
         stop_event = threading.Event()
-        processors = [awesomeProcessor(str(uuid), threading.Event()) for uuid in range(300)]
+        processors = [awesomeProcessor(str(uuid), threading.Event()) for uuid in range(8)]
         print(f"started {len(processors)} processors")
 
-        data = [Data(number, context) for number, context in (generate_data(100_000))]
+        data = [Data(number, context) for number, context in (generate_data(3_000))]
+        print(f"processing {len(data)} data points")
         while len(data) > 0:
             processor = random.choice(processors)
-            random_index = random.randint(0, len(data)-1)
+            random_index = random.randint(0, len(data) - 1)
             data_point = data.pop(random_index)
             processor.receive_request(data_point)
 
         [processor.join() for processor in processors]
         print("----------------------------------")
         end_time = time.time()
-        print(f"that took {end_time-start_time}")
+        print(f"that took {end_time - start_time}")
