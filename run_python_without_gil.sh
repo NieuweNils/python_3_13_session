459812124dae9b735b8966b54a3978f6c183cb1d
nils <nils.nieuwenkamp@eneco.com>
2024-08-05 15:36:03 +0200

    fix bug bash scripts

--- run_python_without_gil.sh
+++ run_python_without_gil.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-docker build . -t cpython:3.13_gil_disabled --build-arg gil=gil_disabled
-docker run -it cpython:3.13_gil_disabled bash
+docker build . -t cpython:3.13_gil_disabled --build-arg gil=disabled
+docker run -it cpython:3.13_gil_disabled python free_threading.py
